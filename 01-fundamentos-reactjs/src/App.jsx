9ba4177dbf05728ec274c29c90351ef269bf9e99
Leandro Amaral <lramaral02m@gmail.com>
2023-08-19 00:07:46 -0300

    Finalizando aulas de criação da sidebar

---
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -0,0 +1,33 @@
+import { Header } from './components/Header';
+import { Post } from './Post';
+
+import styles from './App.module.css';
+
+import './global.css';
+import { Sidebar } from './components/Sidebar';
+
+export function App() {
+  return (
+    <div>
+        <Header />
+
+        <div className={styles.wrapper}>
+          <Sidebar />
+          <main>
+            <Post 
+              author="Leandro Amaral" 
+              content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus voluptatibus iure porro accusamus enim, nam vitae dolor animi, ea repudiandae officiis! Alias ipsam et perspiciatis recusandae neque dicta officiis itaque" 
+            />
+
+            <Post
+              author="Michelle Amaral"
+              content="Um novo post legal"
+            />
+          </main>
+        </div>
+    </div>
+  ) 
+}
+
+
+
