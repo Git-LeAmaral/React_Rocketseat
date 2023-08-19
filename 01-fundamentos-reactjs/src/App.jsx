f50c15eaf1c00b70ca2fb582253e7357c57a267a
Leandro Amaral <lramaral02m@gmail.com>
2023-08-19 17:54:27 -0300

    Iterando no JSX e propriedades do Post

--- 01-fundamentos-reactjs/src/App.jsx
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-key */
 import { Header } from './components/Header';
 import { Post } from './components/Post';
 import { Sidebar } from './components/Sidebar';
@@ -5,6 +6,41 @@ import { Sidebar } from './components/Sidebar';
 import styles from './App.module.css';
 
 import './global.css';
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'http://github.com/LuisBraga31.png',
+      name: 'Luiz Braga',
+      role: 'Web Developer'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋', },
+
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀', },
+
+      { type: 'link', content: '👉 Luiz.design/doctorcare' },
+    ],
+    publishedAt: new Date('2023-08-17 9:35:12')
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'http://github.com/Git-LeAmaral.png',
+      name: 'Leandro Amaral',
+      role: 'Full stack Developer'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋', },
+
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀', },
+
+      { type: 'link', content: '👉 Leandro.design/doctorcare' },
+    ],
+    publishedAt: new Date('2023-08-19 15:55:12')
+  },
+];
 
 export function App() {
   return (
@@ -14,15 +50,15 @@ export function App() {
         <div className={styles.wrapper}>
           <Sidebar />
           <main>
-            <Post 
-              author="Leandro Amaral" 
-              content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus voluptatibus iure porro accusamus enim, nam vitae dolor animi, ea repudiandae officiis! Alias ipsam et perspiciatis recusandae neque dicta officiis itaque" 
-            />
-
-            <Post
-              author="Michelle Amaral"
-              content="Um novo post legal"
-            />
+            {posts.map(post => {
+                return (
+                <Post  
+                  author={post.author}
+                  content={post.content}
+                  publishedAt={post.publishedAt}
+                />
+                )
+            })}
           </main>
         </div>
     </div>
